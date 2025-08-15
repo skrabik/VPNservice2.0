438f513be4cc945b6d4d9b100073afbb0f4251f3
Roman <babenk00r@yandex.ru>
2025-08-15 23:58:32 +0300

    feat sofdeletes for customers and subscriptions

--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -5,9 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Subscription extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         'customer_id',
         'plan_id',
